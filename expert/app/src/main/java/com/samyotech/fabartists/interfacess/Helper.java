6df031c998d775d4e54da4cb75cc34c548cca3f6
AustineGwa <austinegwa64@gmail.com>
2020-10-24 18:47:13 +0300

    weekend builds

--- expert/app/src/main/java/com/samyotech/fabartists/interfacess/Helper.java
+++
@@ -1,9 +0,0 @@
-package com.samyotech.fabartists.interfacess;
-
-import org.json.JSONObject;
-/**
- * Created by VARUN on 01/01/19.
- */
-public interface Helper {
-    public void backResponse(boolean flag, String msg, JSONObject response);
-}
